075e793a68abbfe401a25dc1f4edaa70c0c307c9
Juan Olivares <jdop2000@gmail.com>
2020-12-09 20:48:45 +0000

    0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Tels if all boxes can be opened """
+    stack = []
+    nodeSeen = []
+    node = 0
+
+    while node not in nodeSeen and node is not None:
+        nodeSeen.append(node)
+        for key in boxes[node]:
+            if key not in nodeSeen:
+                stack.append(key)
+        print("stack ----> ", stack)
+        if stack:
+            node = stack[-1]
+            print("node ---->", node)
+            stack.pop()
+        else:
+            break
+    if len(nodeSeen) == len(boxes):
+        return True
+    else:
+        return False
