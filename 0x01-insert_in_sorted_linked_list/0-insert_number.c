44d2fcdad2cf364b0f3c2a0af125ab8288edfa22
Juan Olivares <jdop2000@gmail.com>
2020-12-16 21:17:13 +0000

    Documenting code

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -13,15 +13,17 @@ listint_t *insert_node(listint_t **head, int number){
 	listint_t *current = *head;
 	listint_t *temp, *new;
 
-	if (head[0] == NULL){
+	// if list is empty add new node
+	if (*head == NULL){
 		new = malloc(sizeof(listint_t));
 		new->n = number;
 		new->next = NULL;
-		head[0] = new;
+		*head = new;
 		return new;
 	}
 
-	if (number <= head[0]->n){
+	// if n is less than new, add new node at head
+	if (number <= (*head)->n){
 		new = malloc(sizeof(listint_t));
 		new->n = number;
 		new->next = *head;
@@ -29,6 +31,7 @@ listint_t *insert_node(listint_t **head, int number){
 		return new;
 	}
 
+	// trverse the list until find a node grater than new node or tail
 	while (number > current->n && current->next != NULL){
 		temp = current;
 		current = current->next;
@@ -37,12 +40,14 @@ listint_t *insert_node(listint_t **head, int number){
 	new = malloc(sizeof(listint_t));
 	new->n = number;
 
+	// if new node is grater than hole list, add new node at tail
 	if (current->next == NULL && number > current->n){
 		new->next = NULL;
 		current->next = new;
 		return new;
         }
 
+	// add node in between the list
 	new->next = current;
 	temp->next = new;
 	return new;
