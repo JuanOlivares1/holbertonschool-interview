92de1f219cbe8a856a58efbe7e8eb0fc378e5b9d
Juan Olivares <jdop2000@gmail.com>
2020-12-16 21:27:49 +0000

    Documenting code, adding betty coding style

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,52 +3,51 @@
 #include "lists.h"
 
 /**
- * insert_node
+ * insert_node - Function that insert a new node in a sorted singly linked list
  * @head: pointer to lists' head
  * @number: number to inster into list
  *
  * Return: pointer to new node
  */
-listint_t *insert_node(listint_t **head, int number){
+listint_t *insert_node(listint_t **head, int number)
+{
 	listint_t *current = *head;
 	listint_t *temp, *new;
-
-	// if list is empty add new node
-	if (*head == NULL){
+	/* if list is empty add new node */
+	if (*head == NULL)
+	{
 		new = malloc(sizeof(listint_t));
 		new->n = number;
 		new->next = NULL;
 		*head = new;
-		return new;
+		return (new);
 	}
-
-	// if n is less than new, add new node at head
-	if (number <= (*head)->n){
+	/* if n is less than new, add new node at head */
+	if (number <= (*head)->n)
+	{
 		new = malloc(sizeof(listint_t));
 		new->n = number;
 		new->next = *head;
 		*head = new;
-		return new;
+		return (new);
 	}
-
-	// trverse the list until find a node grater than new node or tail
-	while (number > current->n && current->next != NULL){
+	/* trverse the list until find a node grater than new node or tail */
+	while (number > current->n && current->next != NULL)
+	{
 		temp = current;
 		current = current->next;
 	}
 
 	new = malloc(sizeof(listint_t));
 	new->n = number;
-
-	// if new node is grater than hole list, add new node at tail
-	if (current->next == NULL && number > current->n){
+	/* if new node is grater than hole list, add new node at tail */
+	if (current->next == NULL && number > current->n)
+	{
 		new->next = NULL;
 		current->next = new;
-		return new;
-        }
-
-	// add node in between the list
+		return (new);
+	}
 	new->next = current;
 	temp->next = new;
-	return new;
+	return (new);
 }
