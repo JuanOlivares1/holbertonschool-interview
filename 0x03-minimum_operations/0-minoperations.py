15010eddaa2f79e2b156190feecbffd981ef1967
JuanOlivares1 <jdop2000@gmail.com>
2021-01-07 17:02:56 -0500

    first commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+	Main file for testing
+"""
+
+
+def minOperations(n):
+	"""	minOperations """
+	i = 2
+	a_list = []
+
+	if type(n) is not int and n < 2:
+		return 0
+
+	while n > 1:
+		if n % i == 0:
+			n /= i
+			a_list.append(i)
+		else:
+			i += 1
+
+	return (sum(a_list))
