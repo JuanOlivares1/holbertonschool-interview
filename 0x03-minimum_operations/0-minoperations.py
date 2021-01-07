2bde194827ded7c8f3df519b0e6280662af87065
JuanOlivares1 <jdop2000@gmail.com>
2021-01-07 17:06:35 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,18 +5,17 @@
 
 
 def minOperations(n):
-	"""	minOperations """
-	i = 2
-	a_list = []
+    """	minOperations """
+    i = 2
+    a_list = []
 
-	if type(n) is not int and n < 2:
-		return 0
+    if type(n) is not int and n < 2:
+        return 0
 
-	while n > 1:
-		if n % i == 0:
-			n /= i
-			a_list.append(i)
-		else:
-			i += 1
-
-	return (sum(a_list))
+    while n > 1:
+        if n % i == 0:
+            n /= i
+            a_list.append(i)
+        else:
+            i += 1
+    return (sum(a_list))
