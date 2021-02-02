a2093681b590d3217725ebc61d6423b9337093ce
JuanOlivares1 <jdop2000@gamil.com>
2021-02-02 14:48:06 -0500

    cycle in linked list

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if linked list has a cycle.
+ * @list: head of list
+ *
+ * Return: Always 0.
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *tortoise = list;
+    listint_t *hare = list;
+    
+    if (list == NULL)
+        return (0);
+
+    while (tortoise != NULL && hare != NULL)
+    {
+        tortoise = tortoise->next;
+        hare= hare->next->next;
+
+        if (tortoise == hare)
+            return (1);
+    }
+
+    return (0);
+}
