a9a013e5dc2fa773f8fa20c70a2e37cdb630c0b5
JuanOlivares1 <jdop2000@gmail.com>
2021-05-19 20:15:04 -0500

    last try

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -72,18 +72,18 @@ int is_stable(int grid[3][3])
  * @grid: 3x3 grid
  *
  */
-static void print_grid(int grid[3][3])
+void print_grid(int grid[3][3])
 {
 	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
-	for (j = 0; j < 3; j++)
-	{
-		if (j)
-			printf(" ");
-		printf("%d", grid[i][j]);
-	}
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
 	printf("\n");
 	}
 }
