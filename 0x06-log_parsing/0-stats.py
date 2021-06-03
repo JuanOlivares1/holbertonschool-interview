c83cfd3dda0ac236365c9cf70c154e97cbab6e23
JuanOlivares1 <jdop2000@gmail.com>
2021-06-03 15:01:04 -0500

    fixing error

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,6 +9,8 @@ s_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
 try:
     for count, log in enumerate(sys.stdin, 1):
         splt = log.split(" ")
+        if len(splt) < 2:
+            continue
         if splt[-2] in s_codes:
             s_codes[splt[-2]] += 1
         t_size += int(splt[-1])
