d2559ace2fa009f847b2a83aa86b17e64fb1ec18
JuanOlivares1 <jdop2000@gmail.com>
2021-06-25 18:44:46 -0500

    utf-validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" """
+
+
+def validUTF8(data):
+    """
+    Args:
+        data (list): data set of characters 
+    """
+    t_bytes = 0
+
+    for i in data:
+        byte = format(i, '#010b')[-8:]
+        if t_bytes == 0:
+            if byte[0] == '1':
+                t_bytes = len(byte.split('0')[0])
+
+            if t_bytes == 1 or t_bytes > 4:
+                return False
+
+            elif t_bytes == 0:
+                continue
+        else:
+            if not byte.startswith('10'):
+                return False
+        t_bytes = t_bytes - 1
+
+    if t_bytes != 0:
+        return False
+    return True
+ 
