a8280dc205060aaafadc68a4f7f4e4d03910b47c
JuanOlivares1 <jdop2000@gmail.com>
2021-07-01 15:34:22 -0500

    slide

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,14 @@
+#ifndef cc
+#define SLIDE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SLIDE_LEFT 76
+#define SLIDE_RIGHT 82
+
+int slide_line(int *line, size_t size, int direction);
+int slide_left(int *line, size_t size);
+int slide_right(int *line, size_t size);
+
+#endif //SLIDE_RIGHT
