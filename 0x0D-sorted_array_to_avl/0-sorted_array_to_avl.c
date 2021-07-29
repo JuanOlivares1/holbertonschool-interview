44d6e6300d0907c26201934fd3c16eb48e531e15
JuanOlivares1 <jdop2000@gmail.com>
2021-07-29 18:07:22 -0500

    first try

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,5 +8,73 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	int len = size;
+	int mid = (len - 1) / 2;
+	avl_t *root, *temp, *extra;
 
+	root = malloc(sizeof(avl_t));
+	if (root == NULL)
+		return (NULL);
+
+	if (len == 1)
+	{
+		root->n = array[len];
+		return (root);
+	}
+
+	root->n = array[mid];
+	root->left = Tree_builder(root, array, mid);
+	root->right = Tree_builder(root, &array[mid + 1], mid);
+
+	if (len % 2 == 0)
+	{
+		temp = root;
+		while (temp->right != NULL)
+		{
+			temp = temp->right;
+		}
+		extra = malloc(sizeof(avl_t));
+		if (root == NULL)
+			return (NULL);
+		extra->n = array[len - 1];
+		extra->parent = temp;
+		temp->right = extra;
+	}
+	return (root);
+}
+
+/**
+ * Tree_builder - AVL binary tree
+ *
+ * @root: pointer to root node
+ * @array: pointer to array of items
+ * @endidx: end inde of array
+ */
+avl_t *Tree_builder(avl_t *root, int *array, int endidx)
+{
+	int mid = endidx / 2;
+	avl_t *new;
+
+	new = malloc(sizeof(avl_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (endidx == 1)
+	{
+		if (array[0] == root->n)
+			return (NULL);
+
+		new->n = array[0];
+		new->parent = root;
+		new->left = NULL;
+		new->right = NULL;
+		return (new);
+	}
+
+	new->n = array[mid];
+	new->parent = root;
+	new->left = Tree_builder(new, array, mid);
+	new->right = Tree_builder(new, &array[mid + 1], mid);
+
+	return (new);
 }
