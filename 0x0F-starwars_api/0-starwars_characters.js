2759d0137aea1764b47028556611cf93891c1cb7
JuanOlivares1 <jdop2000@gmail.com>
2021-08-16 16:18:31 -0500

    async request to SWAPI

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,20 +4,20 @@ const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 function getData (url) {
-    return new Promise((resolve, reject) => {
-        request(url, (err, response, body) => {
-            if (err) reject(err);
-            resolve(JSON.parse(body));
-        });
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) reject(err);
+      resolve(JSON.parse(body));
     });
+  });
 }
 
 async function getNames () {
-    const res = await getData(url);
-    res.characters.forEach(async function (character) {
-        const chrtr = await getData(character);
-        console.log(chrtr.name);
-    });
+  const res = await getData(url);
+  res.characters.forEach(async function (character) {
+    const chrtr = await getData(character);
+    console.log(chrtr.name);
+  });
 }
 
 getNames();
